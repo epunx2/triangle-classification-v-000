c36846d8651bebb46609a9c33cad686808030e9b
Eric Chrobak <eric@ericchrobak.com>
2018-06-16 19:00:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
+  attr_accessor :sides
+  def initialization(a, b, c)
+    @sides = [a,b,c]
+  end
+
+  def kind
+    
+    if @sides[0] == @sides[1] && @sides[0] == @sides[2]
+      @type = :equilateral
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    elsif()
+
+    end
+    @type
+      
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This violates the triangle inequality principle"
+    end
+  end
 end
