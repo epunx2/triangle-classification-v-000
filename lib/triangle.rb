30f0669e12ca5aaaa69ac101c54e33a52f4a5ba4
Eric Chrobak <eric@ericchrobak.com>
2018-06-16 19:23:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     if @side_one <= 0 && @side_two <= 0 && @side_three <= 0
       raise TriangleError
     elsif @side_one < 0 || @side_two < 0 || @side_three < 0
-      
+      raise TriangleError
     elsif @side_one == @side_two && @side_one == @side_three
       type = :equilateral
     elsif @side_one == @side_two || @side_one == @side_three || @side_two == @side_three
